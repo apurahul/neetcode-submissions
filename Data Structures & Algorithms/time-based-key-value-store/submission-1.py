b131bef13e17b2ad96164d1e74be1aee9cbdf783
Apurwa Rahulkar <apurahul@uw.edu>
2026-04-26 12:55:55 -0700

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,31 @@
+class TimeMap:
+
+    def __init__(self):
+        self.logging = defaultdict(list)
+    
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.logging[key].append([value, timestamp])
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        if key not in self.logging:
+            return res
+        
+        values = self.logging[key]
+
+        l , r = 0, len(values) - 1
+        while l <= r :
+            m = l +  ((r -l )// 2)
+
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+
+        return res
+            
+
+        
