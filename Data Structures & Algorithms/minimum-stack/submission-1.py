56a559ce4ce7a85fe222a3bc9cccc5fee2a8fde5
Apurwa Rahulkar <apurahul@uw.edu>
2026-04-30 19:29:05 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,34 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+        
+
+    def pop(self) -> None:
+            self.stack.pop()
+
+        
+
+    def top(self) -> int:
+            return self.stack[-1]
+
+        
+
+    def getMin(self) -> int:
+        mini = self.stack[-1]
+        temp = []
+
+        while len(self.stack):
+            mini = min(self.stack[-1], mini)
+            temp.append(self.stack.pop())
+
+        while len(temp):
+            self.stack.append(temp.pop())
+
+        return mini
+        
